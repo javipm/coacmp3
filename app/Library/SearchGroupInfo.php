d92ff831328e5a4903c369f9a8c397c7557d1e9b
Javier Pardo <javipm@gmail.com>
2023-08-28 11:53:37 +0200

    Update SearchGroupInfo.php

--- app/Library/SearchGroupInfo.php
+++ app/Library/SearchGroupInfo.php
@@ -13,18 +13,37 @@ class SearchGroupInfo
             return false;
         }
 
-        $info = [];
+        $result = [];
+
+        $groupSearchString = $group;
+        $url = env('APP_AUDIO_SCRAPPER_BASE_URL', '').$groupSearchString;
+
+        $lyrics = [];
+        $music = [];
+        $director = '';
+        $city = '';
+
+        if (! $lyrics || ! $music || ! $director || ! $city) {
+            return false;
+        }
 
         // NOT UPLOADED TO GITHUB: Scrapper group information :)
         // For obvious reasons, this part of the code is not published in the repository
 
         // EXAMPLE:
         // $browser = new HttpBrowser(HttpClient::create());
-        // $crawler = $browser->request('GET', 'https://www.symfony.com/blog/');
-        // $crawler->filter('h2 > a')->each(function ($node) {
-        //     echo $node->text()."\n";
+        // $crawler = $browser->request('GET', $url);
+        // $crawler->filter('h2')->first()->each(function ($node) {
+        //     $lyrics = $node->text();
         // });
 
-        return $info;
+        $result = [
+            'lyrics' => $lyrics,
+            'music' => $music,
+            'director' => $director,
+            'city' => $city,
+        ];
+
+        return $result;
     }
 }
