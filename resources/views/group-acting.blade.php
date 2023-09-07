8ca64a4a0d7f234967415a99e8deb5fd18bfdae4
Javier Pardo <javipm@gmail.com>
2023-09-07 19:12:21 +0200

    fix title

--- resources/views/group-acting.blade.php
+++ resources/views/group-acting.blade.php
@@ -2,8 +2,11 @@
     <section class="mt-2 mb-2">
         {{ Breadcrumbs::render('group', $group) }}
     </section>
+
     <section class="container w-full mx-auto h-full items-center">
-        <div class="md:grid grid-cols-3 gap-10 ">
+        <h1 class="font-title text-2xl text-orange-600 mb-8">{{$group->modality->name}} {{ $group->name }}</h1>
+
+        <div class="xl:grid grid-cols-3 gap-10 ">
             <div class="col-span-1 mb-10 md:mb-0 shadow-md hover:shadow-xl rounded-lg overflow-hidden">
                 <x-group.card :group="$group" />
                 <x-group.audio-player :actings="$actings" :initialSong="$initialSong"
@@ -11,7 +14,6 @@
             </div>
 
             <article class="col-span-2 mb-10 md:mb-0 [&>p]:text-sm [&>p]:pb-4 [&>p]:text-gray-600">
-                <h1 class="font-title text-2xl text-orange-600 mb-8">{{$group->modality->name}} {{ $group->name }}</h1>
                 {!! $group->description !!}
             </article>
         </div>
