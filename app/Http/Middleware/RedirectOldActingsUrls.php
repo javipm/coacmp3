fc2dfc7a1d1724e189e6119504a1a32869ea10fd
Javier Pardo <javipm@gmail.com>
2023-09-10 19:47:27 +0200

    middleware redirect antiguas descargas

---
+++ app/Http/Middleware/RedirectOldActingsUrls.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\GroupActing;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RedirectOldActingsUrls
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        //Redirect old downloads URLs to new URLs
+        if ($request->has('file')) {
+            $file = $request->input('file');
+            if ($file) {
+                $file = base64_decode($file);
+                if (preg_match("#\.(mp3)$# i", $file)) {
+                    $acting = GroupActing::where('filename', $file)->first();
+                    if ($acting) {
+                        return redirect()->route('group-acting', ['modality' => $acting->modality->slug, 'year' => $acting->year, 'group' => $acting->group->slug, 'phase' => $acting->phase]);
+                    }
+                }
+            }
+        }
+
+        return $next($request);
+    }
+}
