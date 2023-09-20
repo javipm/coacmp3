de816a2b73f84d2e959bb2a92f2b51918c6ecc33
Javier Pardo <javipm@gmail.com>
2023-09-20 10:27:55 +0200

    fix spinner city

--- app/Library/SpinnerTextDescriptions.php
+++ app/Library/SpinnerTextDescriptions.php
@@ -8,15 +8,15 @@ use App\Models\GroupActing;
 class SpinnerTextDescriptions
 {
     const TEXT_TEMPLATES = [
-        0 => 'Disfruta de la actuación de la >modality <strong>>name</strong> en la >phase del COAC >year. Este grupo, originario de Cádiz, ha sido creado por >authors_lyrics, con la música compuesta por >authors_music. Desde este sitio web, tienes la opción de escuchar su actuación de manera gratuita a través de nuestro reproductor o descargarla en formato MP3 para disfrutarla cómodamente desde cualquier dispositivo.',
+        0 => 'Disfruta de la actuación de la >modality <strong>>name</strong> en la >phase del COAC >year. Este grupo, originario de >city, ha sido creado por >authors_lyrics, con la música compuesta por >authors_music. Desde este sitio web, tienes la opción de escuchar su actuación de manera gratuita a través de nuestro reproductor o descargarla en formato MP3 para disfrutarla cómodamente desde cualquier dispositivo.',
 
-        1 => 'Escucha la actuación de la >modality <strong>>name</strong> en la fase >phase del COAC >year. Esta agrupación, que proviene de Cádiz, ha sido escrita por >authors_lyrics, y cuenta con la música de >authors_music. Desde esta página, puedes disfrutar de su actuación de forma gratuita a través de nuestro reproductor o descargarla en formato MP3 para escucharlo en cualquier dispositivo.',
+        1 => 'Escucha la actuación de la >modality <strong>>name</strong> en la fase >phase del COAC >year. Esta agrupación, que proviene de >city, ha sido escrita por >authors_lyrics, y cuenta con la música de >authors_music. Desde esta página, puedes disfrutar de su actuación de forma gratuita a través de nuestro reproductor o descargarla en formato MP3 para escucharlo en cualquier dispositivo.',
 
-        2 => 'Goza de la actuación ofrecida por la >modality <strong>>name</strong> en la fase >phase del COAC >year. Esta destacada agrupación, oriunda de Cádiz, ha sido creada por >authors_lyrics, y su música es obra de >authors_music. En esta plataforma, tienes la opción de disfrutar de su actuación de manera gratuita mediante nuestro reproductor o descargarla en formato MP3 para su comodidad en cualquier dispositivo.',
+        2 => 'Goza de la actuación ofrecida por la >modality <strong>>name</strong> en la fase >phase del COAC >year. Esta destacada agrupación, oriunda de >city, ha sido creada por >authors_lyrics, y su música es obra de >authors_music. En esta plataforma, tienes la opción de disfrutar de su actuación de manera gratuita mediante nuestro reproductor o descargarla en formato MP3 para su comodidad en cualquier dispositivo.',
 
-        3 => 'Deléitate con la actuación de la >modality <strong>>name</strong> en la fase >phase del COAC >year. Este grupo, con origen en Cádiz, ha sido concebido por >authors_lyrics, y la música ha sido compuesta por >authors_music. Desde este sitio web, tienes la posibilidad de escuchar su actuación de forma gratuita utilizando nuestro reproductor o descargarla en formato MP3 para tu comodidad en cualquier dispositivo.',
+        3 => 'Deléitate con la actuación de la >modality <strong>>name</strong> en la fase >phase del COAC >year. Este grupo, con origen en >city, ha sido concebido por >authors_lyrics, y la música ha sido compuesta por >authors_music. Desde este sitio web, tienes la posibilidad de escuchar su actuación de forma gratuita utilizando nuestro reproductor o descargarla en formato MP3 para tu comodidad en cualquier dispositivo.',
 
-        4 => 'Disfruta de la actuación brindada por la >modality <strong>>name</strong> en la fase >phase del COAC >year. Esta agrupación, que tiene su origen en Cádiz, ha sido creada por >authors_lyrics, con la música a cargo de >authors_music. Desde esta página, tienes la opción de escuchar su actuación de manera gratuita utilizando nuestro reproductor o descargarla en formato MP3 para su conveniencia en cualquier dispositivo.',
+        4 => 'Disfruta de la actuación brindada por la >modality <strong>>name</strong> en la fase >phase del COAC >year. Esta agrupación, que tiene su origen en >city, ha sido creada por >authors_lyrics, con la música a cargo de >authors_music. Desde esta página, tienes la opción de escuchar su actuación de manera gratuita utilizando nuestro reproductor o descargarla en formato MP3 para su conveniencia en cualquier dispositivo.',
     ];
 
     public static function getForActing(GroupActing $acting): string
@@ -37,6 +37,7 @@ class SpinnerTextDescriptions
             '>phase' => strtolower($acting->phase),
             '>authors_lyrics' => $acting->group->authorsLyrics->pluck('name')->join(', ', ' y '),
             '>authors_music' => $acting->group->authorsMusic->pluck('name')->join(', ', ' y '),
+            '>city' => $acting->group->city,
         ];
 
         $description = strtr($template, $vars);
@@ -63,6 +64,7 @@ class SpinnerTextDescriptions
             'en la fase >phase' => '',
             '>authors_lyrics' => $group->authorsLyrics->pluck('name')->join(', ', ' y '),
             '>authors_music' => $group->authorsMusic->pluck('name')->join(', ', ' y '),
+            '>city' => $group->city,
         ];
 
         $description = strtr($template, $vars);
