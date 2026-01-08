b9c0ee33ac93a5f635707ba5f01ed89cf1faa97d
Javier Pardo <javier@amarillolimon.net>
2026-01-08 08:49:49 +0100

    2026

--- app/Livewire/Tables/LastActingsTable.php
+++ app/Livewire/Tables/LastActingsTable.php
@@ -97,6 +97,6 @@ class LastActingsTable extends DataTableComponent
 
     public function builder(): Builder
     {
-        return GroupActing::query()->select('*')->where('created_at', '>=', now()->year)->orderBy('created_at', 'desc');
+        return GroupActing::query()->select('*')->whereYear('created_at', now()->year)->orderBy('created_at', 'desc');
     }
 }
